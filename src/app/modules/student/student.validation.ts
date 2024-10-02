ce46a6114f6bda86ad379f8196c1b4457e953369
Noyon Hossain <noyonhossain560@gmail.com>
2024-10-02 08:25:40 +0600

    validateRequest higherOrder function added

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
-const userNameValidationSchema = z.object({
+const createUserNameValidationSchema = z.object({
     firstName: z.string(),
     middleName: z.string().optional(),
     lastName: z.string()
 });
 
-const guardianValidationSchema = z.object({
+const createGuardianValidationSchema = z.object({
     fatherName: z.string(),
     fatherOccupation: z.string(),
     fatherContactNo: z.string(),
@@ -15,33 +15,35 @@ const guardianValidationSchema = z.object({
     motherContactNo: z.string()
 });
 
-const localGuardianValidationSchema = z.object({
+const createLocalGuardianValidationSchema = z.object({
     name: z.string(),
     occupation: z.string(),
     contactNo: z.string(),
     address: z.string()
 });
 
-const studentValidationSchema = z.object({
-    id: z.string(),
-    name: userNameValidationSchema,
-    gender: z.enum(['male', 'female', 'other']),
-    dateOfBirth: z.string(),
-    email: z.string().email("Invalid email address"),
-    contactNo: z.string(),
-    emergencyContactNo: z.string(),
-    BloodGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], {
-        errorMap: () => ({ message: "Invalid blood group" })
-    }),
-    presentAddress: z.string(),
-    permanentAddress: z.string(),
-    guardian: guardianValidationSchema,
-    localGuardian: localGuardianValidationSchema,
-    profileImg: z.string().optional(),
-    isActive: z.enum(['active', 'blocked'], {
-        errorMap: () => ({ message: "Status must be either active or blocked" })
-    }).default('active'),
-    isDeleted: z.boolean().default(false)
+const createStudentValidationSchema = z.object({
+    body: z.object({
+        password: z.string().max(20),
+        student: z.object({
+            name: createUserNameValidationSchema,
+            gender: z.enum(['male', 'female', 'other']),
+            dateOfBirth: z.string(),
+            email: z.string().email("Invalid email address"),
+            contactNo: z.string(),
+            emergencyContactNo: z.string(),
+            BloodGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'], {
+                errorMap: () => ({ message: "Invalid blood group" })
+            }),
+            presentAddress: z.string(),
+            permanentAddress: z.string(),
+            guardian: createGuardianValidationSchema,
+            localGuardian: createLocalGuardianValidationSchema,
+            profileImg: z.string().optional(),
+        })
+    })
 });
 
-export default studentValidationSchema; 
+export const StudentValidations = {
+    createStudentValidationSchema
+} 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { UserController } from "./user.controller";
+import { StudentValidations } from "../student/student.validation";
+import validateRequest from "../../middlewares/validateRequest";
 
 const router = express.Router();
 
-router.post('/create-student', UserController.createStudent);
+router.post('/create-student', validateRequest(StudentValidations.createStudentValidationSchema), UserController.createStudent);
 
 export const UserRoutes = router;
