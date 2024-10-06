daeb628f6bb8ff00377c55b9ebf1b99e8d2346b7
Noyon Hossain <noyonhossain560@gmail.com>
2024-10-06 09:19:06 +0600

    Custom error pattern implement

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,14 +1,45 @@
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSources } from "../interface/error";
+import config from "../config";
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
-    const message = err.message || 'Something went wrong!';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    let statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+    let message = err.message || 'Something went wrong!';
+    let errorSources: TErrorSources = [{
+        path: '',
+        message: 'Something went wrong!'
+    }]
+
+    const handleZodError = (err: ZodError) => {
+        const statusCode = 400;
+        const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
+            return {
+                path: issue.path[issue.path.length - 1],
+                message: issue.message
+            }
+        })
+
+        return {
+            statusCode,
+            message: "Validation Error",
+            errorSources,
+        }
+    }
+
+    if (err instanceof ZodError) {
+        const simplefiedError = handleZodError(err);
+        statusCode = simplefiedError?.statusCode;
+        message = simplefiedError?.message;
+        errorSources = simplefiedError?.errorSources;
+    }
 
     res.status(statusCode).json({
         sucess: false,
         message,
-        error: err
+        errorSources,
+        stack: config.NODE_ENV === 'development' ? err?.stack : null
     })
 }
 
