75140ea36f91e3e1e11f5f2abce0eed6b2b636e1
Noyon Hossain <noyonhossain560@gmail.com>
2024-10-23 08:03:39 +0600

    Verify token using auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,15 +1,25 @@
 import httpStatus from "http-status";
 import AppError from "../errors/AppError";
 import catchAsync from "../utils/catchAsync";
+import jwt from "jsonwebtoken";
+import config from "../config";
 
 const auth = () => {
     return catchAsync(async (req, res, next) => {
         const token = req.headers.authorization;
-        if(!token){
+        if (!token) {
             throw new AppError(httpStatus.UNAUTHORIZED, "Access denied. No token provided.")
         }
 
-        return next();
+        jwt.verify(token, config.jwt_access_secret as string, function (err, decoded) {
+            if (err) {
+                throw new AppError(httpStatus.UNAUTHORIZED, "Invalid token");
+            }
+
+            req.user = decoded;
+            next();
+        });
+        
     });
 }
 
