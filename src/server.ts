bd747cb32eb1449fc3124ddec2fc91475a26e63e
Noyon Hossain <noyonhossain560@gmail.com>
2024-11-01 08:33:52 +0600

    seed Super Admin

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import app from "./app";
 import mongoose from "mongoose";
 import config from "./app/config";
 import { Server } from "http";
+import seedSuperAdmin from "./app/DB";
 
 let server: Server;
 
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
+        seedSuperAdmin();
 
         server = app.listen(config.port, () => {
             console.log(`PH-HealthCare Server listening on port ${config.port}`)
